85063c6635bf529d90ae5b6fdb9603c942b5315b
ies2018 <38878029+ies2018@users.noreply.github.com>
2018-07-22 14:40:18 +0300

    Add files via upload

--- excel-merge.py
+++ excel-merge.py
@@ -21,25 +21,29 @@ import numpy as np
 data_files = glob.glob(r'C:\TARS\AAAActive\EQ8_Gap_Analysis_2018\NewRegs\*.xlsx')         
 
 ### 2. Forms them into a list (data_files)
-print (" ".join(map(str,data_files)))
+print ("\n".join(map(str,data_files)))
 
 print("\nTotal files found is ",len(data_files))
 
-### 3. Reads each file into dataframe
-all_data = pd.DataFrame()
-for f in data_files:
-    df = pd.read_excel(f)
+check_input = input("Continue with merge? (y/n) ")
+
+if check_input == "y":
+
+    ### 3. Reads each file into dataframe
+    all_data = pd.DataFrame()
+    for f in data_files:
+        df = pd.read_excel(f)
+        
+        ### 4. Drops the first row
+        df = df.drop([0])
+        all_data = all_data.append(df,ignore_index=True)
     
-    ### 4. Drops the first row
-    df = df.drop([0])
-    all_data = all_data.append(df,ignore_index=True)
-
-### 5. Converts empty cells to 0    
-all_data.fillna(0)
-
-### 6. Saves the combined dataframe as a new Excel worksheet file
-output_file = 'master_reg.xlsx'
-os.remove(output_file)    
-writer = pd.ExcelWriter(output_file)
-all_data.to_excel(writer)
-writer.save()
+    ### 5. Converts empty cells to 0    
+    all_data.fillna(0)
+    
+    ### 6. Saves the combined dataframe as a new Excel worksheet file
+    output_file = 'master_reg.xlsx'
+    os.remove(output_file)    
+    writer = pd.ExcelWriter(output_file)
+    all_data.to_excel(writer)
+    writer.save()
